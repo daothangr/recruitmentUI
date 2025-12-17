1bfc58fde803709580da33da92b0553beebced69
Dao Hong Thang <daohongthang0101@gmail.com>
2025-12-18 00:44:43 +0700

    comment

--- js/pagination.js
+++ js/pagination.js
@@ -11,7 +11,7 @@ const nextBtn = document.getElementById("nextPage");
 let currentPage = 1;
 let pageSize = Number(pageSizeSelect.value);
 
-
+// Get All Data
 function getAllData() {
     return JSON.parse(localStorage.getItem("candidateList")) || [];
 }
@@ -19,17 +19,22 @@ function getAllData() {
 let allData = getAllData();
 totalRecord.textContent = allData.length;
 
+
+// Update Button Disabled Style for Pagination
 function updateButtonStyle(button, disabled) {
     button.disabled = disabled;
     button.style.backgroundColor = disabled ? "#d1d5db" : "#7a8188"; 
     button.style.cursor = disabled ? "not-allowed" : "pointer";
 }
 
+// Paginate
 function paginate() {
+    // Take Data for Current Page
     const start = (currentPage - 1) * pageSize;
     const end = start + pageSize;
-
     const pageData = allData.slice(start, end);
+
+    // Render Candidate Data
     renderCandidateTable(pageData);
 
 
@@ -37,15 +42,18 @@ function paginate() {
     const to = Math.min(end, allData.length);
     recordRange.textContent = `${from} - ${to} bản ghi`;
 
+    // Update Button Styles
     updateButtonStyle(prevBtn, currentPage === 1);
     updateButtonStyle(nextBtn, end >= allData.length);
 }
 
+// Event Listeners to Pagination Controls
 pageSizeSelect.addEventListener("change", () => {
     pageSize = Number(pageSizeSelect.value);
     currentPage = 1;
     paginate();
 });
+
 
 prevBtn.addEventListener("click", () => {
     if (currentPage > 1) {
@@ -64,6 +72,7 @@ nextBtn.addEventListener("click", () => {
 
 paginate();
 
+// Restart Pagination When Data Changes
 export function restartPagination() {
     currentPage = 1;
     allData = getAllData();
